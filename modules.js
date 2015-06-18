d3ab044a96689a413b7e68e063cbfe00bd51f2d3
Sam Hutchings <samuel.hutchings@btinternet.com>
2015-06-18 14:58:08 +0100

    Added module to process remaining playback time for VTs and display it on the camera client countdown.

--- modules.js
+++ modules.js
@@ -3,4 +3,5 @@ module.exports = function(http){
 
     var atem = require('./lib/atem')(io);
     var clock = require('./lib/timeofday')(io);
+    var caspar-countdown = require('./lib/caspar-countdown')(io);
 }
--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,10 @@ function getURLParameter(sParam)
 {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
+    for (var i = 0; i < sURLVariables.length; i++)
     {
         var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
+        if (sParameterName[0] == sParam)
         {
             return sParameterName[1];
         }
@@ -16,11 +16,10 @@ $(function() {
     window.scrollTo(0,1);
     var inId = parseInt(getURLParameter('in'));
     $('#cam span').text(inId);
-    
+
     var socket = io();
 
     socket.on('camera tally', function(inputNumber, state){
-        console.log(inputNumber, state);
         if (state.program){
             if (isNaN(inId)){
                 $('#cam span').text(inputNumber);
@@ -29,7 +28,7 @@ $(function() {
         if( inputNumber === parseInt(inId)){
             if (state.program){
                 $('#display').css("background", "#EF4136");  // Program
-                
+
             } else if (state.preview){
                 $('#display').css("background", "#71BF4B");  // Preview
             } else {
@@ -37,9 +36,26 @@ $(function() {
             }
         }
     });
-    
+
     socket.on('timeofday', function(timestring){
-        console.log(timestring);
         $('#timeofday span').text(timestring);
     });
+
+    socket.on('cg countdown path', function(path){
+        $('#note boxtitle p').text(path);
+    });
+
+    socket.on('cg countdown timeData', function(remainingTime, totalTime){
+
+        var minutes = Math.floor(remainingTime / 60);
+        var seconds = remainingTime - (minutes * 60);
+
+        seconds = seconds.toFixed(2);
+
+        minutes = (minutes < 10 ? "0" : "") + minutes;
+        seconds = (seconds < 10 ? "0" : "") + seconds;
+
+        $('#note span').text(minutes + ':' + seconds);
+    });
+
 });
