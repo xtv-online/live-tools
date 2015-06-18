9c2ac3906d5b818bc225e27ee73db1ce8ee9a0e0
Niklas Rahmel <niklas@rahmel.eu>
2015-06-18 15:37:11 +0100

    Typo & Clear-Up

--- modules.js
+++ modules.js
@@ -3,5 +3,5 @@ module.exports = function(http){
 
     var atem = require('./lib/atem')(io);
     var clock = require('./lib/timeofday')(io);
-    var caspar-countdown = require('./lib/caspar-countdown')(io);
+    var casparCountdown = require('./lib/caspar-countdown')(io);
 }
