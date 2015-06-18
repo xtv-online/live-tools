c5ea5f18a1d42098897d2392b1e4397dd377c4c0
Niklas Rahmel <niklas@rahmel.eu>
2015-06-18 22:17:16 +0100

    Intercom Mute per Client from RX

--- public/js/intercom.js
+++ public/js/intercom.js
@@ -4,10 +4,12 @@ $(window).on('beforeunload', function(){
 
 var localstream;
 
+// connect to broker for 
+var peer = new Peer(getURLParameter('id'), {key: PEERJSKEY});
+
 $(document).ready(function() {
-    $( "#startCall" ).click(function() {
-        startCall($('#destination').val());
-    });
+    // init microphone
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
     
     // save stream object pointer
     navigator.getUserMedia(
@@ -20,6 +22,61 @@ $(document).ready(function() {
       }
     );
     
+    // open peer connection
+    peer.on('open', function(id)
+    {
+        $('#welcome').text("Connected as: " + id); // show peer ID in DOM to show connect success
+    });
+    
+    // RX VOX
+    peer.on('call', function(call) {
+        console.log('Received Call: ', call);
+        call.answer(localstream); // Answer the call with an audio stream.
+        call.on('stream', function(remoteStream) {
+            console.log('Playing remote stream ', remoteStream);
+
+            // create new <audio> object in #playback for playback
+            jQuery('<audio/>', {
+                id: call.peer + '-player',
+                autoplay: true,
+            }).appendTo('#playback');
+            audio = document.getElementById(call.peer + '-player');
+            audio.src = URL.createObjectURL(remoteStream);
+            document.getElementById(call.peer + '-player').muted = true;
+        });        
+    });
+    
+    // RX CONTROL
+    peer.on('connection', function(conn) {
+        console.log('connection');
+        conn.on('data', function(data){
+            document.getElementById(conn.peer + '-player').muted ? document.getElementById(conn.peer + '-player').muted = false : document.getElementById(conn.peer + '-player').muted = true;
+            console.log(conn.peer, data, document.getElementById(conn.peer + '-player').muted);
+        });
+    });
+
+    
+    $( "#startCall" ).click(function() {
+        startCall($('#destination').val());
+        
+        // TX CONTROL
+        
+        // todo: instead of 'toggle', send 'mute' or 'unmute' in case message gets lost in transfer..
+        $('<button class="togglemute btn btn-danger" id="' + $('#destination').val() + '-mute"' + '>' + $('#destination').val() + '</button>').bind("click", toggleMuteClick).appendTo('#txcontrol');
+        
+        function toggleMuteClick() {
+            idToMute = $(this).attr('id').substring(0, $(this).attr('id').length - 5);
+            console.log("Muting", this.id);
+            conn = peer.connect(idToMute, reliable = true);
+            console.log('Sending toggle to', idToMute, conn);
+            conn.on('open', function(){
+                conn.send('Toggle');
+            });
+            $(this).toggleClass( "btn-danger" );
+            $(this).toggleClass( "btn-success" );
+        };
+    });
+
 });
 
 function getURLParameter(sParam)
@@ -36,13 +93,20 @@ function getURLParameter(sParam)
     }
 }
 
+// TX
 function startCall(destination)
 {
     console.log('Calling ', destination, ' with ', localstream);
     var call = peer.call(destination, localstream);
     call.on('stream', function(remoteStream) {
-      console.log('Playing remote stream ', remoteStream);
-      audio.src = URL.createObjectURL(remoteStream);
+        console.log('Playing remote stream ', remoteStream);
+          
+        // create new <audio> object in #playback for playback
+        jQuery('<audio/>', {
+            id: destination + '-player',
+            autoplay: true,
+        }).appendTo('#playback');
+        audio = document.getElementById(destination + '-player');
+        audio.src = URL.createObjectURL(remoteStream);
     });
-    
 }
