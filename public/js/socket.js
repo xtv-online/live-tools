3eb6a8c5e598ec80e5e4c1f5cb442ab292ace666
Sam Hutchings <samuel.hutchings@btinternet.com>
2015-06-19 16:03:10 +0100

    Added files for configuration

---
+++ public/js/socket.js
@@ -0,0 +1,8 @@
+var socket = io();
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
