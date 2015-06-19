0f7b7d30ade6a80e784262f5d5671ed4b475e56a
Niklas Rahmel <niklas@rahmel.eu>
2015-06-19 22:40:16 +0100

    handshake ui

--- public/js/socket.js
+++ public/js/socket.js
@@ -3,7 +3,15 @@ $(function() {
     
     socket.on("who are you", function(roles){
         console.log(roles);
-        socket.emit("i am", "Camera 2");
+        roles.forEach(function(role) {
+            $('<option value=' + role._id + '>' + role.roleName + '</select>').appendTo('#roles');
+            console.log("adding", role.roleName);
+        });
     });
  
+ 
+    $( ".confirmRole" ).click(function() {
+        console.log("selected", $( "#roles option:selected" ).val());
+        socket.emit("i am", $( "#roles option:selected" ).val());
+    });
 });
