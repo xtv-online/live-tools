93ac037b6e9fe17bd5829502d0dd55637a615182
Sam Hutchings <samuel.hutchings@btinternet.com>
2015-06-19 17:25:32 +0100

    Added template for new module structure Added beginnings of a connection/client management module

--- modules.js
+++ modules.js
@@ -1,6 +1,10 @@
 module.exports = function(http) {
     var io = require('socket.io')(http);
 
+    var clientManager = require('./lib/client-manager');
+    clientManager.initialise(io);
+    clientManager.start();
+
     var atem = require('./lib/atem');
     atem.initialise(io);
     atem.start();
