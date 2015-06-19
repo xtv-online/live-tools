2a9b7c8d8cbaabbf8df536d0312641b7c4932fe4
Sam Hutchings <samuel.hutchings@btinternet.com>
2015-06-19 17:12:19 +0100

    Corrected new module structure to allow for live module reloading.

--- modules.js
+++ modules.js
@@ -2,11 +2,13 @@ module.exports = function(http) {
     var io = require('socket.io')(http);
 
     var atem = require('./lib/atem');
-    atem.start(io);
+    atem.initialise(io);
+    atem.start();
 
     var clock = require('./lib/timeofday')(io);
 
     var casparCountdown = require('./lib/caspar-countdown');
-    casparCountdown.start(io);
+    casparCountdown.initialise(io);
+    casparCountdown.start();
 
 };
