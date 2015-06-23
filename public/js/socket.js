4ed4ee735aca0f9046aa10fa5afebdae5f56426f
Sam Hutchings <samuel.hutchings@btinternet.com>
2015-06-23 18:58:17 +0100

    Client Modifications Added all socket message events for handshaking on client Added status messages in DOM for testing

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,38 +1,32 @@
 $(function() {
     var socket = io();
-    
+
     socket.on("who are you", function(roles){
-        console.log(roles);
         roles.forEach(function(role) {
             $('<option value=' + role._id + '>' + role.roleName + '</select>').appendTo('#roles');
-            console.log("adding", role.roleName);
         });
     });
-    
-    
-    // unnecessary unless want to build interface all on one page w// jquery
-    /*     
-    socket.on("build interface", function(rows){ // rows = [["rowId": "row1", "height": "11", "divs":[["divId": "tally", "title": "Input", "rowId": "row1", "columns": "4"]]]]
-        rows.forEach(function(row) {
-            addRow(row.rowId, row.height);
-            row.divs.forEach(function(div) {
-                buildDiv(row.rowId, row.height);
-            });
-        });
-    }); */
- 
+
     $( ".confirmRole" ).click(function() {
-        console.log("selected", $( "#roles option:selected" ).val());
         socket.emit("i am", $( "#roles option:selected" ).val());
     });
+
+    socket.on("client connected", function(client) {
+        console.log('Client Connected: ' + client.role[0].roleName);
+        $('.messages').prepend('<p>' + '<b>Client Connected: </b>' + client.role[0].roleName + '</p>');
+    });
+
+    socket.on("client disconnected", function(client) {
+        console.log('Client Disconnected: ' + client.role[0].roleName);
+        $('.messages').prepend('<p>' + '<b>Client Disconnected: </b>' + client.role[0].roleName + '</p>');
+    });
+
+    socket.on("invalid identity", function () {
+        console.log('Invalid identity provided');
+    });
+
+    socket.on("you are", function (clientDescription) {
+        console.log('You are:', clientDescription);
+    });
+
 });
-
-
-// unnecessary unless want to build interface all on one page w// jquery
-/* function buildDiv(divId, title, rowId, columns) {
-    $('<div id="' + divId + '" class=" modulebox col-xs-' + columns + '"><boxtitle><p>' + title + '</p></boxtitle><span></span></div>').appendTo('#' + rowId + ''');
-}
-
-function addRow(rowId, height) {
-    $('<div id="' + rowId + '" style="height:' + height + 'em;"class="row"></div>').appendTo('#view');
-} */
