01cef0ecbb847da82b654e3f87447afdc0bdcfaa
Sam Hutchings <samuel.hutchings@btinternet.com>
2015-06-23 21:48:09 +0100

    Client-side Handshaking Update Updated handshaking interface, provided connection feedback, connection status Implemented cookie system for session based client role retainment

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,32 +1,46 @@
 $(function() {
     var socket = io();
 
-    socket.on("who are you", function(roles){
+    socket.on('who are you', function(roles){
         roles.forEach(function(role) {
             $('<option value=' + role._id + '>' + role.roleName + '</select>').appendTo('#roles');
         });
+
+        if ($.cookie('live-tools-identity') !== undefined) {
+            socket.emit('i am', $.cookie('live-tools-identity'));
+        }
     });
 
-    $( ".confirmRole" ).click(function() {
-        socket.emit("i am", $( "#roles option:selected" ).val());
+    $( '.confirmRole' ).click(function() {
+        socket.emit('i am', $( '#roles option:selected' ).val());
     });
 
-    socket.on("client connected", function(client) {
+    $( '.storeInCookie' ).click(function() {
+        var identityID = $( '#roles option:selected' ).val();
+        $.cookie('live-tools-identity', identityID);
+    });
+
+    $( '.deleteCookie' ).click(function() {
+        $.removeCookie('live-tools-identity');
+    });
+
+    socket.on('client connected', function(client) {
         console.log('Client Connected: ' + client.role[0].roleName);
-        $('.messages').prepend('<p>' + '<b>Client Connected: </b>' + client.role[0].roleName + '</p>');
+        $('.messages').prepend('<p><b>' + client.role[0].roleName + '</b>' + ' connected</p>');
     });
 
-    socket.on("client disconnected", function(client) {
+    socket.on('client disconnected', function(client) {
         console.log('Client Disconnected: ' + client.role[0].roleName);
-        $('.messages').prepend('<p>' + '<b>Client Disconnected: </b>' + client.role[0].roleName + '</p>');
+        $('.messages').prepend('<p><b>' + client.role[0].roleName + '</b>' + ' disconnected</p>');
     });
 
-    socket.on("invalid identity", function () {
+    socket.on('invalid identity', function () {
         console.log('Invalid identity provided');
     });
 
-    socket.on("you are", function (clientDescription) {
+    socket.on('you are', function (clientDescription) {
         console.log('You are:', clientDescription);
+        $('span#identity').text(clientDescription.role[0].roleName);
     });
 
 });
