0844a46646639401bb12bd88d60091885485054f
Sam Hutchings <samuel.hutchings@btinternet.com>
2015-06-26 19:31:35 +0100

    Updates of Multiview View

--- public/views/multiview/js/main.js
+++ public/views/multiview/js/main.js
@@ -1,29 +1,42 @@
 $(function() {
-    var date = new Date(); updateClock(date);
-    // update text clock
-    var update = setInterval(function(){ date = new Date(); updateClock(date); }, 1000);
 
+    handshaking_module(ready);
+
+    function ready() {
+
+        // Set up time of day
+        timeofday_module('', updateClock);
+
+        // Setup Caspar Countdown Module
+        casparcountdown_module('#countdown', '#countdownTitle span');
+
+        // Setup Custom Countdown
+        customcountdown_module('#countdown', '#countdownTitle span');
+
+    }
 
     $('.segment').textfill({});
 });
 
 
 
-function updateClock(date) {
+function updateClock(timeString) {
+    var date = new Date(timeString);
+
     hours = date.getHours();
     minutes = date.getMinutes();
     seconds = date.getSeconds();
 
     // update text
-    $( "#hm" ).text( ((hours < 10) ? "0" + hours : hours) + ":" + ((minutes < 10) ? "0" + minutes : minutes) );
-    $( "#s" ).text( ((seconds < 10) ? "0" + seconds : seconds));
+    $("#hm").text(((hours < 10) ? "0" + hours : hours) + ":" + ((minutes < 10) ? "0" + minutes : minutes));
+    $("#s").text(((seconds < 10) ? "0" + seconds : seconds));
 
     // light up seconds
     for (i = 0; i < (seconds + 1); i++) {
-        $( ".p" + i ).css( "background", "#EF4136" );
+        $(".p" + i).css("background", "#EF4136");
     }
     // clear remaining seconds
     for (i = (seconds + 1); i < 60; i++) {
-        $( ".p" + i ).css( "background", "gray" );
+        $(".p" + i).css("background", "gray");
     }
 }
