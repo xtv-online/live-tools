97362391270549e405106e3a329a28e1a87eefe1
Niklas Rahmel <niklas@rahmel.eu>
2015-06-28 21:48:03 +0100

    Intercom: Camera Control and Call Initiation

--- public/views/director/js/main.js
+++ public/views/director/js/main.js
@@ -44,7 +44,10 @@ $(function() {
         messaging_module_initialise(newMessage, newAcknowledgement);
 
         // Setup RX time display
-        txtime_module('#livetimer', '#livestatusText', '#livestatus');        
+        txtime_module('#livetimer', '#livestatusText', '#livestatus'); 
+
+        // Setup intercom controllers
+        intercom_control_module();
             
         // init countdown
         $("#cdtoggle").trigger( "click" );
@@ -109,11 +112,6 @@ $(function() {
     
 });
 
-
-var localstream;
-var clients = new Object();
-var peer;
-
 /* NEED:
   * unique ID of this (director) client
   * unique ID of all clients per role
@@ -123,75 +121,3 @@ var peer;
 */
 
 // connect to broker for peer ID
-
-
-
-function connectToBroker(id){
-    peer = new Peer(id, {key: PEERJSKEY});
-
-    // init microphone
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-    // save stream object pointer
-    navigator.getUserMedia(
-      {video: false, audio: true},
-      function(stream) {
-        localstream = stream;
-      },
-      function(err) {
-        console.log('Failed to get local stream' ,err);
-    });
-};
-
-function addClient(peerId, role) {
-    if(!clients.hasOwnProperty(role)){
-        clients[role] = new Array();
-        addButtons(role);
-    };
-    clients[role].push(peerId);
-};
-
-function addButtons(role){
-    // add Tally
-    $('<div class="point" id="' + role + '-tally"></div>').appendTo('#tallyIndicator');
-    // add TX button
-    $('<button class="btn-circle-lg btn btn-success  txButton" id="' + role + '-muteTx"' + '>' + role + '</button>').bind("click", muteTx).appendTo('#txControl');
-    // add RX button
-    $('<button class="btn-circle-lg btn btn-success rxButton" id="' + role + '-muteRx"' + '>' + role + '</button>').bind("click", muteRx).appendTo('#rxControl');
-    // add Status
-    $('<div class="point" id="' + role + '-statu"></div>').appendTo('#statusIndicator');
-};
-
-function muteTx() {
-    roleToMute = $(this).attr('id').substring(0, $(this).attr('id').length - 7);
-    // send mute to all clients in role, wait for one to ACK and then change colour
-}
-
-function muteRx() {
-    roleToMute = $(this).attr('id').substring(0, $(this).attr('id').length - 7);
-    // mute locally all players for role and then change colour
-}
-
-function startCall(destination)
-{
-    var call = peer.call(destination, localstream);
-    call.on('stream', function(remoteStream) {
-        // create new <audio> object in #playback for playback
-        jQuery('<audio/>', {
-            id: destination + '-player',
-            autoplay: true,
-        }).appendTo('#playback');
-        audio = document.getElementById(destination + '-player');
-        audio.src = URL.createObjectURL(remoteStream);
-    });
-    // send signal to all clients that not muted
-};
-
-function callAllClients(){
-    var i;
-    for (key in clients){
-        for    (i = 0; i < clients[key].length; i++) {
-            startCall(clients[key][i]);
-        };
-    };
-};
